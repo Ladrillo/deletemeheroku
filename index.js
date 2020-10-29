8005f6d54d5543071353d49ebe349abafbe4e854
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-29 15:55:06 +0100

    wip

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 require('dotenv').config()
 
+const path = require('path')
 const express = require('express')
 
 const server = express()
 
 const PORT = express.env.PORT || 4400
 
-server.use(express.static(__dirname + '/client/build'))
+server.use(express.static(path.join(__dirname, 'build')))
 
 server.listen(PORT, () => {
   console.log(`server started on ${PORT}`)
 })
+
+server.get('/api', (req, res) => {
+  res.json({ message: 'foo' })
+})
+
+const express = require('express');
+const path = require('path');
+const app = express();
